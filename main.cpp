5043c28de88915b2625a9e5c6cc5897cda72e178
ElliottF05 <elliotthfaa@gmail.com>
2024-05-13 17:46:36 -0400

    Fully working point rendering

--- main.cpp
+++ main.cpp
@@ -4,8 +4,10 @@
 #include <SFML/System/Vector2.hpp>
 #include <SFML/Window/Window.hpp>
 #include <SFML/Window/WindowStyle.hpp>
+#include <cmath>
 #include <iostream>
-#include <format>
+#include <chrono>
+#include <thread>
 #include "sfml/Graphics.hpp"
 
 
@@ -17,6 +19,7 @@ void drawPoint(sf::RenderWindow& window, const sf::Vector2f& pos);
 // Classes / Structs
 struct Vec3;
 struct Camera;
+struct displayVec;
 
 struct Vec3 {
     float x,y,z;
@@ -82,7 +85,9 @@ struct Vec3 {
         //std::cout << "after rotations: " << toString();
     }
 
-    sf::Vector2f toScreenCoords(const Camera& cam);
+    displayVec toPlaneCoords(const Camera& cam);
+    sf::Vector2f toScreenCoords(const Camera& cam, sf::RenderWindow& window);
+    void draw(const Camera& cam, sf::RenderWindow& window);
 
     std::string toString() {
         return std::to_string(this->x) + ", " + std::to_string(this->y) + ", " + std::to_string(this->z) + "\n";
@@ -95,33 +100,61 @@ struct Vec3 {
 
 struct Camera {
     Vec3 pos;
-    float thetaY, thetaZ;
+    float thetaY, thetaZ, fov, fov_rad;
 
-    Camera(Vec3 pos, float thetaY, float thetaZ) {
+    Camera(Vec3 pos, float thetaY, float thetaZ, float fov) {
         this->pos = pos;
         this->thetaY = thetaY;
         this->thetaZ = thetaZ;
+        this->fov = fov;
+        this->fov_rad = fov * M_PI / 180.0;
     };
 };
 
-sf::Vector2f Vec3::toScreenCoords(const Camera& cam) {
+struct displayVec : Vec3 {
+    bool inView;
+
+    displayVec(float x, float y, bool inView) : Vec3(x, y, 0) {
+        this->inView = inView;
+    }
+};
+
+displayVec Vec3::toPlaneCoords(const Camera& cam) {
     Vec3 rotated = Vec3(*this);
 
-    std::cout << toString();
+    // std::cout << toString();
 
     rotated.subtract(cam.pos);
     rotated.rotate(-cam.thetaZ, -cam.thetaY);
 
-    std::cout << toString();
+    // std::cout << rotated.toString();
 
-    // TODO: ADD FOV CALCULATIONS, remove placeholder mult
-    int mult = 100;
-    return sf::Vector2f(mult * rotated.y / rotated.x, mult * rotated.z / rotated.x);
+    return displayVec(rotated.y / rotated.x, rotated.z / rotated.x, (rotated.x > 0) ? true : false);
+}
+
+sf::Vector2f Vec3::toScreenCoords(const Camera& cam, sf::RenderWindow& window) {
+    displayVec planeCoords = toPlaneCoords(cam);
+    // std::cout << "planeCoords: " << planeCoords.x << ", " << planeCoords.y << "\n";
+    float maxPlaneCoordValue = tan(0.5 * cam.fov_rad);
+
+    float x, y;
+    if (planeCoords.inView) {
+        x = window.getSize().x * (planeCoords.x / maxPlaneCoordValue + 0.5);
+        y = 0.5 * window.getSize().y - window.getSize().x * planeCoords.y / maxPlaneCoordValue;
+    } else {
+        x = 0;
+        y = 0;
+    }
+
+    return sf::Vector2f(x, y);
+}
+
+void Vec3::draw(const Camera& cam, sf::RenderWindow& window) {
+    drawPoint(window, toScreenCoords(cam, window));
 }
 
 
 int main(int, char**){
-    std::cout << "Hello, from 3D-Graphics-Engine!\n";
 
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
@@ -130,42 +163,37 @@ int main(int, char**){
     window.setFramerateLimit(60); // call it once, after creating the window
 
     //testing some vectors
-    Camera cam = Camera(Vec3(0, 0, 0), 0, 0);
+    Camera cam = Camera(Vec3(0, 0, 0), 0, 0, 90);
     Vec3 v1 = Vec3(1,0,0);
     Vec3 v2 = Vec3(10,2,0);
-    Vec3 v3 = Vec3(5,0,3);
+    Vec3 v3 = Vec3(10,0,3);
 
     // run the program as long as the window is open
-    bool shouldRender = true;
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
+        cam.thetaZ += 0.01;
+
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
         // clear the window with black color
-        if (shouldRender) {
-            window.clear(sf::Color::Black);
+        window.clear(sf::Color::Black);
 
-            // draw everything here...
-            // window.draw(...);
-            // drawLine(window, 200.f, 50.f, 500.f, 500.f);
+        // draw everything here...
+        // window.draw(...);
+        // drawLine(window, 200.f, 50.f, 500.f, 500.f);
+        v1.draw(cam, window);
+        v2.draw(cam, window);
+        v3.draw(cam, window);
 
-            drawPoint(window, v1.toScreenCoords(cam));
-            drawPoint(window, v2.toScreenCoords(cam));
-            drawPoint(window, v3.toScreenCoords(cam));
+        // end the current frame
+        window.display();
 
-            // end the current frame
-            window.display();
-        }
-
-        shouldRender = false;
-
+    std::this_thread::sleep_for(std::chrono::milliseconds(16));
     }
 
     return 0;
@@ -182,9 +210,9 @@ void drawLine(sf::RenderWindow& window, float x1, float y1, float x2, float y2) 
     window.draw(line, 2, sf::Lines);
 }
 
-void drawPoint(sf::RenderWindow& window, const sf::Vector2f& screenPos) {
-    sf::Vector2f displayCoords = sf::Vector2f(window.getSize().x / 2.0 + screenPos.x, window.getSize().y / 2.0 - screenPos.y);
+void drawPoint(sf::RenderWindow& window, const sf::Vector2f& pos) {
     sf::CircleShape shape(5.f);
-    shape.setPosition(displayCoords);
+    shape.setPosition(pos);
     window.draw(shape);
+    // std::cout << pos.x << ", " <<  pos.y << "\n";
 }
