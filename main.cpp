17747e219c1e217b1a022d37b8f80ea2f3bd575c
ElliottF05 <elliotthfaa@gmail.com>
2024-05-15 20:50:33 -0400

    Add function to look left and right

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Vector2.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/Keyboard.hpp>
 #include <SFML/Window/Window.hpp>
 #include <SFML/Window/WindowStyle.hpp>
 #include <cmath>
@@ -19,7 +21,7 @@ void drawPoint(sf::RenderWindow& window, const sf::Vector2f& pos);
 // Classes / Structs
 struct Vec3;
 struct Camera;
-struct displayVec;
+struct DisplayVec;
 
 struct Vec3 {
     float x,y,z;
@@ -85,8 +87,8 @@ struct Vec3 {
         //std::cout << "after rotations: " << toString();
     }
 
-    displayVec toPlaneCoords(const Camera& cam);
-    sf::Vector2f toScreenCoords(const Camera& cam, sf::RenderWindow& window);
+    DisplayVec toPlaneCoords(const Camera& cam);
+    DisplayVec toScreenCoords(const Camera& cam, sf::RenderWindow& window);
     void draw(const Camera& cam, sf::RenderWindow& window);
 
     std::string toString() {
@@ -111,15 +113,46 @@ struct Camera {
     };
 };
 
-struct displayVec : Vec3 {
-    bool inView;
+struct DisplayVec : Vec3 {
+    bool inFront;
 
-    displayVec(float x, float y, bool inView) : Vec3(x, y, 0) {
-        this->inView = inView;
+    DisplayVec(float x, float y, bool inFront) : Vec3(x, y, 0) {
+        this->inFront = inFront;
+    }
+
+    void draw(sf::RenderWindow& window) {
+        drawPoint(window, sf::Vector2f(this->x, this->y));
     }
 };
 
-displayVec Vec3::toPlaneCoords(const Camera& cam) {
+struct Line {
+    Vec3 p1;
+    Vec3 p2;
+
+    Line(Vec3& p1, Vec3& p2) {
+        this->p1 = p1;
+        this->p2 = p2;
+    }
+
+    void draw(const Camera& cam, sf::RenderWindow& window) {
+        DisplayVec v1 = p1.toScreenCoords(cam, window);
+        DisplayVec v2 = p2.toScreenCoords(cam, window);
+
+        if (!v1.inFront && !v2.inFront) {
+            return;
+        } else if (!v1.inFront) {
+            v1.scalarMult(-1);
+            v1.scalarMult(10);
+        } else if (!v2.inFront) {
+            v2.scalarMult(-1);
+            v2.scalarMult(10);
+        }
+
+        drawLine(window, v1.x, v1.y, v2.x, v2.y);
+    };
+};
+
+DisplayVec Vec3::toPlaneCoords(const Camera& cam) {
     Vec3 rotated = Vec3(*this);
 
     // std::cout << toString();
@@ -129,28 +162,23 @@ displayVec Vec3::toPlaneCoords(const Camera& cam) {
 
     // std::cout << rotated.toString();
 
-    return displayVec(rotated.y / rotated.x, rotated.z / rotated.x, (rotated.x > 0) ? true : false);
+    return DisplayVec(rotated.y / rotated.x, rotated.z / rotated.x, (rotated.x > 0) ? true : false);
 }
-
-sf::Vector2f Vec3::toScreenCoords(const Camera& cam, sf::RenderWindow& window) {
-    displayVec planeCoords = toPlaneCoords(cam);
+DisplayVec Vec3::toScreenCoords(const Camera& cam, sf::RenderWindow& window) {
+    DisplayVec planeCoords = toPlaneCoords(cam);
     // std::cout << "planeCoords: " << planeCoords.x << ", " << planeCoords.y << "\n";
     float maxPlaneCoordValue = tan(0.5 * cam.fov_rad);
 
-    float x, y;
-    if (planeCoords.inView) {
-        x = window.getSize().x * (planeCoords.x / maxPlaneCoordValue + 0.5);
-        y = 0.5 * window.getSize().y - window.getSize().x * planeCoords.y / maxPlaneCoordValue;
-    } else {
-        x = 0;
-        y = 0;
-    }
+    float screenX = window.getSize().x * (planeCoords.x / maxPlaneCoordValue + 0.5);
+    float screenY = 0.5 * window.getSize().y - window.getSize().x * planeCoords.y / maxPlaneCoordValue;
 
-    return sf::Vector2f(x, y);
+    return DisplayVec(screenX, screenY, planeCoords.inFront);
 }
-
 void Vec3::draw(const Camera& cam, sf::RenderWindow& window) {
-    drawPoint(window, toScreenCoords(cam, window));
+    DisplayVec v = toScreenCoords(cam, window);
+    if (v.inFront) {
+        v.draw(window);
+    }
 }
 
 
@@ -164,13 +192,19 @@ int main(int, char**){
 
     //testing some vectors
     Camera cam = Camera(Vec3(0, 0, 0), 0, 0, 90);
-    Vec3 v1 = Vec3(1,0,0);
-    Vec3 v2 = Vec3(10,2,0);
-    Vec3 v3 = Vec3(10,0,3);
+    Vec3 v1 = Vec3(10,0, -2);
+    Vec3 v2 = Vec3(10,2,-2);
+    Vec3 v3 = Vec3(11, 1, -2);
+    Vec3 v4 = Vec3(10.5, 1, 2);
+    Line l1 = Line(v1, v2);
+    Line l2 = Line(v1, v3);
+    Line l3 = Line(v1, v4);
+    Line l4 = Line(v2, v3);
+    Line l5 = Line(v2, v4);
+    Line l6 = Line(v3, v4);
 
     // run the program as long as the window is open
     while (window.isOpen()) {
-        cam.thetaZ += 0.01;
 
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
@@ -178,6 +212,14 @@ int main(int, char**){
             // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
                 window.close();
+
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+            cam.thetaZ -= 0.02;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            cam.thetaZ += 0.02;
         }
 
         // clear the window with black color
@@ -186,9 +228,12 @@ int main(int, char**){
         // draw everything here...
         // window.draw(...);
         // drawLine(window, 200.f, 50.f, 500.f, 500.f);
-        v1.draw(cam, window);
-        v2.draw(cam, window);
-        v3.draw(cam, window);
+        l1.draw(cam, window);
+        l2.draw(cam, window);
+        l3.draw(cam, window);
+        l4.draw(cam, window);
+        l5.draw(cam, window);
+        l6.draw(cam, window);
 
         // end the current frame
         window.display();
@@ -215,4 +260,8 @@ void drawPoint(sf::RenderWindow& window, const sf::Vector2f& pos) {
     shape.setPosition(pos);
     window.draw(shape);
     // std::cout << pos.x << ", " <<  pos.y << "\n";
+}
+
+void draw2DTriangle(sf::RenderWindow& window) {
+    
 }
