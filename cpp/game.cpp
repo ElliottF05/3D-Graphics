8c6fbf9324bdd33d02bd02af433a9ef6630e6ffd
ElliottF05 <elliotthfaa@gmail.com>
2024-11-15 22:07:12 -0500

    First working version of depth calculations for each pixel

--- cpp/game.cpp
+++ cpp/game.cpp
@@ -109,15 +109,17 @@ void Game::render() {
             int width = pixelArray.getWidth();
             int height = pixelArray.getHeight();
 
-            float maxPlaneCoord = tan(camera.getFov() / 2);
-            v1.x = (0.5 * width) * (1 - v1.x / maxPlaneCoord) - 0.5;
-            v1.y = 0.5 * (height - v1.y / maxPlaneCoord * width) - 0.5;
+            float maxPlaneCoord = tan(camera.getFov() / 2.0f);
+            maxPlaneCoord = 1;
 
-            v2.x = (0.5 * width) * (1 - v2.x / maxPlaneCoord) - 0.5;
-            v2.y = 0.5 * (height - v2.y / maxPlaneCoord * width) - 0.5;
+            v1.x = (0.5 * width) * (1 - v1.x / maxPlaneCoord);
+            v1.y = 0.5 * (height - v1.y / maxPlaneCoord * width);
 
-            v3.x = (0.5 * width) * (1 - v3.x / maxPlaneCoord) - 0.5;
-            v3.y = 0.5 * (height - v3.y / maxPlaneCoord * width) - 0.5;
+            v2.x = (0.5 * width) * (1 - v2.x / maxPlaneCoord);
+            v2.y = 0.5 * (height - v2.y / maxPlaneCoord * width);
+
+            v3.x = (0.5 * width) * (1 - v3.x / maxPlaneCoord);
+            v3.y = 0.5 * (height - v3.y / maxPlaneCoord * width);
 
             // 2.2) draw triangle
             if (v1.z < 0 || v2.z < 0 || v3.z < 0) {
@@ -140,6 +142,8 @@ void Game::render() {
 }
 
 void Game::fillTriangle(Vec3& v1, Vec3& v2, Vec3& v3, int r, int g, int b) {
+    // depth calculations from https://www.scratchapixel.com/lessons/3d-basic-rendering/rasterization-practical-implementation/visibility-problem-depth-buffer-depth-interpolation.html#:~:text=As%20previously%20mentioned%2C%20the%20correct,z%20%3D%201%20V%200.
+    
     // std::cout << "game.cpp: fillTriangle() called" << std::endl;
     // std::cout << "v1.y = " << v1.y << ", v2.y = " << v2.y << ", v3.y = " << v3.y << std::endl;
     // sort vertices by y (v1 has lowest y, v3 has highest y)
@@ -154,9 +158,9 @@ void Game::fillTriangle(Vec3& v1, Vec3& v2, Vec3& v3, int r, int g, int b) {
     }
 
     // calculate slopes
-    float slope1 = (v2.x - v1.x) / (v2.y - v1.y);
-    float slope2 = (v3.x - v1.x) / (v3.y - v1.y);
-    float slope3 = (v3.x - v2.x) / (v3.y - v2.y);
+    float slope1 = (v2.x - v1.x) / (v2.y - v1.y); // slope of line from v1 to v2
+    float slope2 = (v3.x - v1.x) / (v3.y - v1.y); // slope of line from v1 to v3
+    float slope3 = (v3.x - v2.x) / (v3.y - v2.y); // slope of line from v2 to v3
 
     // calculate starting and ending x values
     float top = std::max(v1.y, 0.0f);
@@ -168,14 +172,25 @@ void Game::fillTriangle(Vec3& v1, Vec3& v2, Vec3& v3, int r, int g, int b) {
     for (int y = round(top); y < round(bottom); y++) {
         int left = x1;
         int right = x2;
+        
+        float q1 = (y - v1.y) / (v2.y - v1.y);
+        float invLeftDepth = (1 / v1.z) * (1 - q1) + (1 / v2.z) * q1;
+
+        float q2 = (y - v1.y) / (v3.y - v1.y);
+        float invRightDepth = (1 / v1.z) * (1 - q2) + (1 / v3.z) * q2;
+
         if (left > right) {
             std::swap(left, right);
         }
         left = std::max(0, left);
         right = std::min(500, right);
-        int baseIndex = y * 500;
         for (int x = left; x < right; x++) {
-            pixelArray.setPixel(baseIndex + x, r, g, b);
+            float q3 = (float) (x - x1) / (x2 - x1);
+            float invDepth = invLeftDepth * (1 - q3) + invRightDepth * q3;
+            float depth = 1 / invDepth;
+
+            depth = depth - floor(depth);
+            pixelArray.setPixel(x, y, depth * r, depth * g, depth * b);
         }
         x1 += slope1;
         x2 += slope2;
@@ -190,14 +205,25 @@ void Game::fillTriangle(Vec3& v1, Vec3& v2, Vec3& v3, int r, int g, int b) {
     for (int y = round(top); y < round(bottom); y++) {
         int left = x1;
         int right = x2;
+
+        float q1 = (y - v2.y) / (v3.y - v2.y);
+        float invLeftDepth = (1 / v2.z) * (1 - q1) + (1 / v3.z) * q1;
+
+        float q2 = (y - v1.y) / (v3.y - v1.y);
+        float invRightDepth = (1 / v1.z) * (1 - q2) + (1 / v3.z) * q2;
+
         if (left > right) {
             std::swap(left, right);
         }
         left = std::max(0, left);
         right = std::min(500, right);
-        int baseIndex = y * 500;
         for (int x = left; x < right; x++) {
-            pixelArray.setPixel(baseIndex + x, r, g, b);
+            float q3 = (float) (x - x1) / (x2 - x1);
+            float invDepth = invLeftDepth * (1 - q3) + invRightDepth * q3;
+            float depth = 1 / invDepth;
+
+            depth = depth - floor(depth);
+            pixelArray.setPixel(x, y, depth * r, depth * g, depth * b);
         }
         x1 += slope3;
         x2 += slope2;
