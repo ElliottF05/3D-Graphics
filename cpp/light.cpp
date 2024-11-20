f17a31f20dd23270d4e6178e10d4494b91257bdd
ElliottF05 <elliotthfaa@gmail.com>
2024-11-20 00:18:48 -0500

    Apply pixel gap / stitching fixes to Light triangle filling

--- cpp/light.cpp
+++ cpp/light.cpp
@@ -89,9 +89,8 @@ void Light::addVerticesToShadowMap(const std::vector<Vec3>& vertices) {
 }
 
 void Light::fillTriangle(Vec3& v1, Vec3& v2, Vec3& v3) {
-    // std::cout << "filling triangle: " << v1.toString() << ", " << v2.toString() << ", " << v3.toString() << std::endl;
     // depth calculations from https://www.scratchapixel.com/lessons/3d-basic-rendering/rasterization-practical-implementation/visibility-problem-depth-buffer-depth-interpolation.html#:~:text=As%20previously%20mentioned%2C%20the%20correct,z%20%3D%201%20V%200.
-    
+
     // sort vertices by y (v1 has lowest y, v3 has highest y)
     if (v1.y > v2.y) {
         std::swap(v1, v2);
@@ -103,8 +102,8 @@ void Light::fillTriangle(Vec3& v1, Vec3& v2, Vec3& v3) {
         std::swap(v1, v2);
     }
 
-    int width = zBuffer.getWidth();
     int height = zBuffer.getHeight();
+    int width = zBuffer.getWidth();
 
     // calculate slopes
     float slope1 = (v2.x - v1.x) / (v2.y - v1.y); // slope of line from v1 to v2
@@ -116,18 +115,22 @@ void Light::fillTriangle(Vec3& v1, Vec3& v2, Vec3& v3) {
     }
 
     // calculate starting and ending x values
-    float top = std::max(v1.y, 0.0f);
+    int top = std::max(static_cast<int>(std::ceil(v1.y)), 0);
     float x1 = slope1 * (top - v1.y) + v1.x;
     float x2 = slope2 * (top - v1.y) + v1.x;
-    float bottom = std::min(v2.y, height-1.0f);
+    int bottom = std::min(static_cast<int>(std::floor(v2.y)), height-1);
 
     // fill top half
-    for (float y = top; y < bottom; y++) {
-        if (std::isnan(y) || std::isinf(y)) {
-            break;
+    for (int y = top; y <= bottom; y++) {
+
+        int left, right;
+        if (x1 < x2) {
+            left = std::max(static_cast<int>(std::ceil(x1)), 0);
+            right = std::min(static_cast<int>(std::floor(x2)), width-1);
+        } else {
+            left = std::max(static_cast<int>(std::ceil(x2)), 0);
+            right = std::min(static_cast<int>(std::floor(x1)), width-1);
         }
-        float left = x1;
-        float right = x2;
         
         float q1 = (y - v1.y) / (v2.y - v1.y);
         float invLeftDepth = (1 / v1.z) * (1 - q1) + (1 / v2.z) * q1;
@@ -135,24 +138,14 @@ void Light::fillTriangle(Vec3& v1, Vec3& v2, Vec3& v3) {
         float q2 = (y - v1.y) / (v3.y - v1.y);
         float invRightDepth = (1 / v1.z) * (1 - q2) + (1 / v3.z) * q2;
 
-        if (left > right) {
-            std::swap(left, right);
-        }
-        left = std::max(0.0f, left);
-        right = std::min(width-1.0f, right);
-        int yInt = std::round(y);
-        for (float x = left; x < right; x++) {
-            if (std::isnan(x) || std::isinf(x)) {
-                break;
-            }
+        for (int x = left; x <= right; x++) {
+
             float q3 = (float) (x - x1) / (x2 - x1);
             float invDepth = invLeftDepth * (1 - q3) + invRightDepth * q3;
             float depth = 1 / invDepth;
 
-            int xInt = std::round(x);
-
-            if (depth < zBuffer.getPixel(xInt, yInt)) {
-                zBuffer.setPixel(xInt, yInt, depth);
+            if (depth < zBuffer.getPixel(x, y)) {
+                zBuffer.setPixel(x, y, depth);
             }
         }
         x1 += slope1;
@@ -160,17 +153,21 @@ void Light::fillTriangle(Vec3& v1, Vec3& v2, Vec3& v3) {
     }
 
     // fill bottom half
-    top = std::max(v2.y, 0.0f);
+    top = std::max(static_cast<int>(std::ceil(v2.y)), 0);
     x1 = slope3 * (top - v2.y) + v2.x;
     x2 = slope2 * (top - v1.y) + v1.x;
-    bottom = std::min(v3.y, height-1.0f);
+    bottom = std::min(static_cast<int>(std::floor(v3.y)), height-1);
 
-    for (float y = top; y < bottom; y++) {
-        if (std::isnan(y) || std::isinf(y)) {
-            break;
+    for (int y = top; y <= bottom; y++) {
+
+        int left, right;
+        if (x1 < x2) {
+            left = std::max(static_cast<int>(std::ceil(x1)), 0);
+            right = std::min(static_cast<int>(std::floor(x2)), width-1);
+        } else {
+            left = std::max(static_cast<int>(std::ceil(x2)), 0);
+            right = std::min(static_cast<int>(std::floor(x1)), width-1);
         }
-        float left = x1;
-        float right = x2;
 
         float q1 = (y - v2.y) / (v3.y - v2.y);
         float invLeftDepth = (1 / v2.z) * (1 - q1) + (1 / v3.z) * q1;
@@ -178,24 +175,14 @@ void Light::fillTriangle(Vec3& v1, Vec3& v2, Vec3& v3) {
         float q2 = (y - v1.y) / (v3.y - v1.y);
         float invRightDepth = (1 / v1.z) * (1 - q2) + (1 / v3.z) * q2;
 
-        if (left > right) {
-            std::swap(left, right);
-        }
-        left = std::max(0.0f, left);
-        right = std::min(width-1.0f, right);
-        int yInt = std::round(y);
-        for (float x = left; x < right; x++) {
-            if (std::isnan(x) || std::isinf(x)) {
-                break;
-            }
+        for (int x = left; x <= right; x++) {
+
             float q3 = (float) (x - x1) / (x2 - x1);
             float invDepth = invLeftDepth * (1 - q3) + invRightDepth * q3;
             float depth = 1 / invDepth;
 
-            int xInt = std::round(x);
-
-            if (depth < zBuffer.getPixel(xInt, yInt)) {
-                zBuffer.setPixel(xInt, yInt, depth);
+            if (depth < zBuffer.getPixel(x, y)) {
+                zBuffer.setPixel(x, y, depth);
             }
         }
         x1 += slope3;
