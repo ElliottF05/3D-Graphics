25aa25af7e8169f6712994a86c22d3f6ea013b8d
ElliottF05 <elliotthfaa@gmail.com>
2025-04-04 18:58:46 -0400

    Remove index of refraction from Ray, use simpler version instead of assuming one of the mediums is always air

--- wasm-graphics/src/graphics/rt.rs
+++ wasm-graphics/src/graphics/rt.rs
@@ -8,12 +8,11 @@ use super::{game::Game, scene::{SceneObject, Sphere}};
 pub struct Ray {
     pub origin: Vec3,
     pub direction: Vec3,
-    pub index_of_refrac: f32,
 }
 
 impl Ray {
-    pub fn new(origin: Vec3, direction: Vec3, index_of_refrac: f32) -> Self {
-        Self { origin, direction, index_of_refrac }
+    pub fn new(origin: Vec3, direction: Vec3) -> Self {
+        Self { origin, direction }
     }
     pub fn at(&self, t: f32) -> Vec3 {
         self.origin + self.direction * t
@@ -111,7 +110,7 @@ impl Game {
         self.camera.vertex_screen_to_world_space(&mut v);
         let direction = (v - origin).normalized();
 
-        Ray::new(origin, direction, 1.0)
+        Ray::new(origin, direction)
     }
 
     fn get_rand_ray_at_pixel(&self, x: usize, y: usize) -> Ray {
@@ -121,7 +120,7 @@ impl Game {
         self.camera.vertex_screen_to_world_space(&mut v);
         let direction = (v - origin).normalized();
 
-        Ray::new(origin, direction, 1.0)
+        Ray::new(origin, direction)
     }
 }
 
@@ -156,7 +155,7 @@ impl Material for Lambertian {
             console_log!("reflected_dir near zero");
         }
 
-        let reflected_ray = Ray::new(hit_record.pos, reflected_dir, ray.index_of_refrac);
+        let reflected_ray = Ray::new(hit_record.pos, reflected_dir);
         let attenuation = hit_record.surface_color;
         return (true, attenuation, reflected_ray)
     }
@@ -186,7 +185,7 @@ impl Material for Metal {
         if reflected_dir.dot(&hit_record.normal) < 0.0 {
             return (false, Vec3::zero(), Ray::default());
         } else  {
-            let reflected_ray = Ray::new(hit_record.pos, reflected_dir, ray.index_of_refrac);
+            let reflected_ray = Ray::new(hit_record.pos, reflected_dir);
             let attenuation = hit_record.surface_color;
             return (true, attenuation, reflected_ray)
         }
@@ -208,25 +207,35 @@ impl Dielectric {
     pub fn new(index_of_refrac: f32) -> Dielectric {
         return Dielectric { index_of_refrac };
     }
+
+    fn reflectance(&self, cos_theta: f32, n1: f32, n2: f32) -> f32 {
+        // use Schlick's approximation: https://en.wikipedia.org/wiki/Schlick%27s_approximation
+        let mut r_0 = (n1 - n2) / (n1 + n2);
+        r_0 = r_0 * r_0;
+
+        return r_0 + (1.0 - r_0) * (1.0 - cos_theta).powi(5);
+    }
 }
 
 impl Material for Dielectric {
     fn scatter(&self, ray: &Ray, hit_record: &HitRecord) -> (bool, Vec3, Ray) {
 
         let attenuation;
+        let n1;
         let n2;
 
         if hit_record.front_face {
+            n1 = 1.0;
             n2 = self.index_of_refrac;
             attenuation = hit_record.surface_color;
         } else {
             // default to index of refraction of air (1.0) if exiting a dielectric
             // also default to no attenuation (check this)
+            n1 = self.index_of_refrac;
             n2 = 1.0;
             attenuation = Vec3::new(1.0, 1.0, 1.0);
         }
 
-        let n1 = ray.index_of_refrac;
         let n1_over_n2 = n1 / n2;
         
         let ray_dir = ray.direction.normalized();
@@ -237,14 +246,15 @@ impl Material for Dielectric {
 
         // total internal reflection
         let cannot_refract = n1_over_n2 * sin_theta > 1.0;
+        let reflectance = self.reflectance(cos_theta, n1, n2);
 
-        let refracted_dir = if cannot_refract {
+        let refracted_dir = if cannot_refract || reflectance > random_float()  {
             ray_dir.reflect(&hit_record.normal)
         } else {
             ray_dir.refract(&hit_record.normal, n1_over_n2)
         };
 
-        let refracted_ray = Ray::new(hit_record.pos, refracted_dir, n2);
+        let refracted_ray = Ray::new(hit_record.pos, refracted_dir);
         return (true, attenuation, refracted_ray);
     }
     fn clone_box(&self) -> Box<dyn Material> {
