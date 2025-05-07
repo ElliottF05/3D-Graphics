993c60db18e2a556f03cc71d61306c312adf093e
ElliottF05 <elliotthfaa@gmail.com>
2025-05-06 21:25:07 -0400

    Add MaterialEditorControls

--- web/src/react_components/SceneControlPanel/EditPanel/EditPanel.tsx
+++ web/src/react_components/SceneControlPanel/EditPanel/EditPanel.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Accordion } from "@/components/ui/accordion";
 import TransformControls from './TransformControls';
+import MaterialEditorControls from './MaterialEditorControls';
 // import SnapSettingsControls from './SnapSettingsControls';
 // import MaterialEditorControls from './MaterialEditorControls';
 
@@ -10,7 +11,7 @@ const EditPanel: React.FC = () => {
         <div className="space-y-0"> {/* Reduced outer spacing if it's inside another content area */}
             <Accordion type="multiple" defaultValue={['transform']} className="w-full">
                 <TransformControls />
-                {/* Add MaterialEditorControls and other controls here as AccordionItems */}
+                <MaterialEditorControls />
             </Accordion>
         </div>
     );
